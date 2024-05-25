c5562fa03defe5269ef0041cde1f3b26b3cf7076
mp1 <miolszewski@outlook.com>
2024-05-25 09:26:29 +0200

    Change title

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Lista zadań'),
     );
   }
 }
